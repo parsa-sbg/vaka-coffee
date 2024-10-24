caf4084bdcf4a2bc89491208b49da2707473be50
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-10-25 01:06:49 +0330

    refactoring components structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/modules/Header/Header";
+import Header from "@/components/layouts/Header/Header";
 import "./globals.css";
 import { Vazirmatn } from "next/font/google"
 
--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../Logo/Logo'
+import Logo from '../../modules/Logo/Logo'
 import SearchBar from './SearchBar'
 import CartIcon from './CartIcon'
 import Link from 'next/link'
