f9ddaa2622b5486125dc9e3b307190bfe2496ace
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-10-26 22:58:42 +0330

    improve bottom nav styles

--- src/components/layouts/BottomNav/BottomNav.tsx
+++ src/components/layouts/BottomNav/BottomNav.tsx
@@ -3,20 +3,20 @@ import React from 'react'
 
 function BottomNav() {
     return (
-        <nav className='lg:hidden z-40 fixed bottom-0 right-0 left-0 grid grid-cols-6 py-3 border gap-1 border-secondary bg-bgColer'>
+        <nav className='lg:hidden z-40 fixed bottom-0 right-0 left-0 grid grid-cols-6 py-5 border-t border-main  gap-1 bg-bgColer'>
 
 
-            <div className='cursor-pointer col-span-2 flex justify-center items-start group border-l'>
-                <Link href={'/account'} className='group-hover:text-main duration-200 text-nowrap'>محصولات</Link>
-            </div>
+            <Link href='' className='cursor-pointer col-span-2 flex justify-center items-start group border-l'>
+                <span className='group-hover:text-main duration-200 text-nowrap'>محصولات</span>
+            </Link>
 
-            <div className='cursor-pointer col-span-2 flex justify-center items-start group'>
-                <Link href={'/account'} className='group-hover:text-main duration-200 text-nowrap'>تخفیف ها🔥</Link>
-            </div>
+            <Link href='' className='cursor-pointer col-span-2 flex justify-center items-start group'>
+                <span className='group-hover:text-main duration-200 text-nowrap'>تخفیف ها🔥</span>
+            </Link>
 
-            <div className='cursor-pointer col-span-2 flex justify-center items-start border-r group'>
-                <Link href={'/account'} className='group-hover:text-main duration-200 text-nowrap'>حساب کاربری من</Link>
-            </div>
+            <Link href='' className='cursor-pointer col-span-2 flex justify-center items-start border-r group'>
+                <span className='group-hover:text-main duration-200 text-nowrap'>اکانت من</span>
+            </Link>
 
         </nav>
     )
