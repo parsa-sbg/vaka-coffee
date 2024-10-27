aeca27b4df7e307797eac370720b3711895e59fc
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-10-28 01:06:15 +0330

    improve header styles

--- src/components/layouts/Header/SearchBar.tsx
+++ src/components/layouts/Header/SearchBar.tsx
@@ -6,7 +6,7 @@ function SearchBar() {
     return (
         <div className='relative w-full md:max-w-96'>
             <input
-                className='w-full bg-secondary pr-2 lg:pr-4 pl-8 py-2 rounded-sm outline-none border-2 border-transparent transition-colors duration-200 focus:border-main'
+                className='w-full bg-secondary pr-2 lg:pr-4 pl-8 py-2 rounded-md outline-none border-2 border-transparent transition-colors duration-200 focus:border-main'
                 type="text"
                 placeholder='جستجوی محصولات' />
             <div className='cursor-pointer p-1 absolute left-2 top-0 bottom-0 my-auto flex items-center justify-center'>
