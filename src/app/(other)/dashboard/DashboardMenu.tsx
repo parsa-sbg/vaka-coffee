ce417e1b01c7f048676706e5f8d63ad2a130ac54
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-10-29 18:15:05 +0330

    create dashboard menu

--- src/app/(other)/dashboard/DashboardMenu.tsx
+++ src/app/(other)/dashboard/DashboardMenu.tsx
@@ -1,8 +1,28 @@
+import Link from 'next/link'
 import React from 'react'
+import DashboardMenuItem from './DashboardMenuItem'
 
 function DashboardMenu() {
+
+
   return (
-    <div className='bg-green-500 h-56'>SideBar</div>
+    <div className=''>
+      <h3 className='font-semibold text-lg bg-secondary py-2 px-4 mb-2 rounded-md border-b border-b-secondary'>حساب کاربری من</h3>
+
+      <ul className='flex flex-col gap-2'>
+
+        <DashboardMenuItem text='پیشخوان' href='/dashboard' />
+        <DashboardMenuItem text='سفارش‌ ها' href='/dashboard/orders' />
+        <DashboardMenuItem text='آدرس ها' href='/dashboard/addresses' />
+        <DashboardMenuItem text='جزئیات حساب' href='/dashboard/account' />
+
+        <li>
+          <button className='text-right py-2 px-4 rounded-md transition-all duration-200 hover:bg-[#0f0f0f] hover:text-main w-full'>خروج</button>
+        </li>
+
+      </ul>
+
+    </div>
   )
 }
 
