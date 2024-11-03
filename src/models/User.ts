b33e88603a284fae8bc1ad9c9486244153d9114b
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-11-03 19:55:43 +0330

    crete User model

---
+++ src/models/User.ts
@@ -0,0 +1,46 @@
+import { UserDocument, UserModel } from "@/types/user";
+import mongoose from "mongoose";
+
+
+const addressSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    family: { type: String, required: true },
+    state: { type: String, required: true },
+    city: { type: String, required: true },
+    address: { type: String, required: true },
+    houseNumber: { type: String, required: true },
+});
+
+const userSchema = new mongoose.Schema<UserDocument>({
+    name: {
+        required: true,
+        type: String,
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+        length: 11
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    address: {
+        type: addressSchema,
+        required: false
+    }
+})
+
+
+
+const UserModel: UserModel = mongoose.models.User || mongoose.model<UserDocument>('User', userSchema)
+
+
+export default UserModel
---
+++ src/types/user.ts
@@ -0,0 +1,23 @@
+import { Document, Model } from "mongoose";
+
+export interface Address {
+    name: string;
+    family: string;
+    state: string;
+    city: string;
+    address: string;
+    houseNumber: string;
+}
+
+export interface User {
+    name: string
+    username: string
+    phone: string
+    password: string
+    address?: Address
+}
+
+export interface UserDocument extends Document, User { }
+
+
+export interface UserModel extends Model<UserDocument> {}
