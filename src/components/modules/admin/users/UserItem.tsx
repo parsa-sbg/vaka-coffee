1aade9fe2ccb4621ee2ce93cb4efe66110e78c61
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-11-12 00:59:15 +0330

    show users in admin panel

---
+++ src/components/modules/admin/users/UserItem.tsx
@@ -0,0 +1,44 @@
+import { UserInterface } from '@/models/User'
+import toPersionNumber from '@/utils/toPersianNubmer'
+import React from 'react'
+
+type props = {
+    isOdd: boolean
+    user: UserInterface
+    number: number
+}
+
+function UserItem({ isOdd, user, number }: props) {
+    return (
+        <tr className={`${isOdd ? 'bg-secondary' : 'bg-[#0f0f0f]'}`}>
+            <th scope="row" className="px-1 py-4 font-medium whitespace-nowrap">
+                <span className='w-full flex justify-center items-center'>{toPersionNumber(number.toString())}</span>
+            </th>
+            <td className="px-3 lg:px-6 py-4">
+                {user.name}
+            </td>
+            <td className="px-3 lg:px-6 py-4">
+                {user.username}
+            </td>
+            <td className="px-3 lg:px-6 py-4">
+                {toPersionNumber(user.phone)}
+            </td>
+
+            <td className="px-3 lg:px-6 py-4">
+                <div className='flex gap-2 text-xs'>
+
+                    <button className={`text-nowrap bg-main text-bgColer font-semibold px-4 md:px-7 py-2 rounded-md transition-all duration-300 ${isOdd ? 'sm:hover:bg-[#0f0f0f]' : 'sm:hover:bg-secondary'}  sm:hover:text-main`} >
+                        مشاهده
+                    </button>
+
+                    <button className={`text-nowrap bg-main text-bgColer font-semibold px-4 md:px-7 py-2 rounded-md transition-all duration-300 ${isOdd ? 'sm:hover:bg-[#0f0f0f]' : 'sm:hover:bg-secondary'}  sm:hover:text-main`} >
+                        حذف
+                    </button>
+
+                </div>
+            </td>
+        </tr>
+    )
+}
+
+export default UserItem
--- src/components/modules/admin/users/UsersTable.tsx
+++ src/components/modules/admin/users/UsersTable.tsx
@@ -1,7 +1,18 @@
+import UserModel from '@/models/User'
+import { connectToDataBase } from '@/utils/server/dataBase'
 import toPersionNumber from '@/utils/toPersianNubmer'
 import React from 'react'
+import UserItem from './UserItem'
 
-function UsersTable() {
+async function UsersTable() {
+
+
+    connectToDataBase()
+
+    const users = await UserModel.find()
+    console.log('users => ', users);
+
+
     return (
         <table className="rounded-md overflow-hidden text-sm text-left rtl:text-right">
 
@@ -10,9 +21,11 @@ function UsersTable() {
                     <th scope="col" className="px-1 py-3">
                         <span>شماره</span>
                     </th>
-
                     <th scope="col" className="px-3 lg:px-6 py-3">
                         نام
+                    </th>
+                    <th scope="col" className="px-3 lg:px-6 py-3">
+                        نام کاربری
                     </th>
                     <th scope="col" className="px-3 lg:px-6 py-3">
                         شماره تماس
@@ -25,83 +38,9 @@ function UsersTable() {
 
             <tbody>
 
-                <tr className="bg-[#0f0f0f]">
-                    <th scope="row" className="px-1 py-4 font-medium whitespace-nowrap">
-                        <span className='w-full flex justify-center items-center'>{toPersionNumber('1')}</span>
-                    </th>
-                    <td className="px-3 lg:px-6 py-4">
-                        پارسا
-                    </td>
-                    <td className="px-3 lg:px-6 py-4">
-                        {toPersionNumber('09123456789')}
-                    </td>
-
-                    <td className="px-3 lg:px-6 py-4">
-                        <div className='flex gap-2 text-xs'>
-
-                            <button className='text-nowrap bg-main text-bgColer font-semibold px-4 md:px-7 py-2 rounded-md transition-all duration-300 sm:hover:bg-secondary sm:hover:text-main' >
-                                مشاهده
-                            </button>
-
-                            <button className='text-nowrap bg-main text-bgColer font-semibold px-4 md:px-7 py-2 rounded-md transition-all duration-300 sm:hover:bg-secondary sm:hover:text-main' >
-                                حذف
-                            </button>
-
-                        </div>
-                    </td>
-                </tr>
-
-                <tr className="bg-[#0f0f0f]">
-                    <th scope="row" className="px-1 py-4 font-medium whitespace-nowrap">
-                        <span className='w-full flex justify-center items-center'>{toPersionNumber('2')}</span>
-                    </th>
-                    <td className="px-3 lg:px-6 py-4">
-                        پارسا
-                    </td>
-                    <td className="px-3 lg:px-6 py-4">
-                        {toPersionNumber('09123456789')}
-                    </td>
-
-                    <td className="px-3 lg:px-6 py-4">
-                        <div className='flex gap-2 text-xs'>
-
-                            <button className='text-nowrap bg-main text-bgColer font-semibold px-4 md:px-7 py-2 rounded-md transition-all duration-300 sm:hover:bg-secondary sm:hover:text-main' >
-                                مشاهده
-                            </button>
-
-                            <button className='text-nowrap bg-main text-bgColer font-semibold px-4 md:px-7 py-2 rounded-md transition-all duration-300 sm:hover:bg-secondary sm:hover:text-main' >
-                                حذف
-                            </button>
-
-                        </div>
-                    </td>
-                </tr>
-
-                <tr className="bg-[#0f0f0f]">
-                    <th scope="row" className="px-1 py-4 font-medium whitespace-nowrap">
-                        <span className='w-full flex justify-center items-center'>{toPersionNumber('3')}</span>
-                    </th>
-                    <td className="px-3 lg:px-6 py-4">
-                        پارسا
-                    </td>
-                    <td className="px-3 lg:px-6 py-4">
-                        {toPersionNumber('09123456789')}
-                    </td>
-
-                    <td className="px-3 lg:px-6 py-4">
-                        <div className='flex gap-2 text-xs'>
-
-                            <button className='text-nowrap bg-main text-bgColer font-semibold px-4 md:px-7 py-2 rounded-md transition-all duration-300 sm:hover:bg-secondary sm:hover:text-main' >
-                                مشاهده
-                            </button>
-
-                            <button className='text-nowrap bg-main text-bgColer font-semibold px-4 md:px-7 py-2 rounded-md transition-all duration-300 sm:hover:bg-secondary sm:hover:text-main' >
-                                حذف
-                            </button>
-
-                        </div>
-                    </td>
-                </tr>
+                {users.map((user, index) => (
+                    <UserItem user={user} number={index + 1} isOdd={index / 2 !== 0} />
+                ))}
 
             </tbody>
         </table>
