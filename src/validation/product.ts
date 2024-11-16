ff97504fff96203d8b071ed6393c0a6184209651
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-11-17 01:30:32 +0330

    crete zod schema to validate new product datas

---
+++ src/validation/product.ts
@@ -0,0 +1,57 @@
+import toPersionNumber from '@/utils/toPersianNubmer'
+import { z } from 'zod'
+
+
+const MAX_FILE_SIZE = 5 * 1024 * 1024;
+const ALLOWED_FILE_TYPES = ["image/jpeg", "image/png", "image/webp"];
+
+const imageFileSchema = z.custom<File>((file) => {
+    return (
+        file instanceof File &&
+        ALLOWED_FILE_TYPES.includes(file.type) &&
+        file.size <= MAX_FILE_SIZE
+    );
+}, {
+    message: toPersionNumber('فرمت عکس های محصول باید jpeg ، png و یا webp و از 5 مگابایت کمتر باشد .'),
+    path: ['pictures']
+});
+
+
+const dynamicFieldSchema = z.object({
+
+    key: z.string()
+        .min(2, 'کلید داینامیک فیلد نباید از دو کاراکتر کمتر باشد.')
+        .max(15, toPersionNumber('کلید داینامیک فیلد نباید از 15 کاراکتر بیشتر باشد.')),
+
+    value: z.string()
+        .min(2, 'مقدار داینامیک فیلد نباید از دو کاراکتر کمتر باشد.')
+        .max(15, toPersionNumber('مقدار داینامیک فیلد نباید از 15 کاراکتر بیشتر باشد.'))
+
+})
+
+export const productSchema = z.object({
+
+    name: z.string()
+        .min(7, toPersionNumber('نام محصول باید حداقل 7 کاراکتر داشته باشد.'))
+        .max(20, toPersionNumber('نام محصول باید حداکثر 20 کاراکتر داشته باشد.')),
+
+
+    price: z.number()
+        .min(0, 'مبلغ نمیتواند از صفر کمتر باشد !'),
+
+
+    discount: z.number()
+        .min(0, 'درصد تخفیف نمیتواند از صفر کمتر باشد !')
+        .max(100, toPersionNumber('درصد تخفیف نمیتواند از 100 بیشتر باشد !')),
+
+
+    dynamicFields: z.array(dynamicFieldSchema),
+
+    stock: z.number()
+        .min(0, 'موجودی انبار نمیتواند از صفر کمتر باشد !'),
+
+    category: z.string().min(1, "دسته‌بندی نمی‌تواند خالی باشد"),
+
+
+    pictures: z.array(imageFileSchema)
+})
