a97f656b383b05626621f08b326001bf11d1f20b
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-12-10 02:51:49 +0330

    fix show all btn for category section

--- src/components/modules/index/CategorySection.tsx
+++ src/components/modules/index/CategorySection.tsx
@@ -15,7 +15,7 @@ function CategorySection({ categoryWithProducts }: categorySectionProps) {
                 <h3 className=' font-bold text-2xl relative before:absolute before:right-0 before:top-0 pr-4 before:bottom-0 before:my-auto before:w-2 before:h-2 before:rounded-full before:bg-main'>
                     {categoryWithProducts.name}
                 </h3>
-                <Button link href={`/categories/${categoryWithProducts.shortName}`} text={`همه ${categoryWithProducts.name} ها`} />
+                <Button link href={`/categories/${categoryWithProducts.shortName}`} text={categoryWithProducts.name.includes('های') || categoryWithProducts.name.includes('ات') ? `همه ${categoryWithProducts.name}` : `همه ${categoryWithProducts.name} ها` } />
             </div>
             <ProductsSlider products={JSON.parse(JSON.stringify(categoryWithProducts.products)) || []} />
         </section>
