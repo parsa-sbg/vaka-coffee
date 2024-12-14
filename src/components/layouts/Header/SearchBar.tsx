d759d25ff3c786261d7cdea92e264f362410f954
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-12-15 01:35:03 +0330

    fix searchbar bug

--- src/components/layouts/Header/SearchBar.tsx
+++ src/components/layouts/Header/SearchBar.tsx
@@ -10,6 +10,12 @@ function SearchBar() {
     return (
         <div className='relative w-full md:max-w-96'>
             <input
+                onKeyUp={(e) => {
+                    if (e.key == 'Enter') {
+                        setSearch('')
+                        search.length && route.push(`/search/${search}`)
+                    }
+                }}
                 value={search}
                 onChange={e => { setSearch(e.target.value) }}
                 className='w-full bg-secondary pr-2 lg:pr-4 pl-8 py-2 rounded-md outline-none border-2 border-transparent transition-colors duration-200 focus:border-main'
@@ -17,7 +23,7 @@ function SearchBar() {
                 placeholder='جستجوی محصولات' />
             <div onClick={() => {
                 setSearch('')
-                route.push(`/search/${search}`)
+                search.length && route.push(`/search/${search}`)
             }} className='cursor-pointer p-1 absolute left-2 top-0 bottom-0 my-auto flex items-center justify-center'>
                 <FaSearch className='' />
             </div>
