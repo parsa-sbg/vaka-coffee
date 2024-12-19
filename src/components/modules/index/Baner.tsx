942a64e92b622c7aaf13208534aa9d1168ef2ed2
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-12-19 17:02:05 +0330

    save banner images in public folder

--- src/components/modules/index/Baner.tsx
+++ src/components/modules/index/Baner.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 function Baner() {
     return (
-        <section className='my-16 py-20 flex items-center justify-center md:justify-start bg-[url(https://vakacoffee.com/wp-content/uploads/2023/03/14011221005.jpg)] object-fill bg-cover bg-no-repeat'>
+        <section className='my-16 py-20 flex items-center justify-center md:justify-start bg-[url(/banner.jpg)] object-fill bg-cover bg-no-repeat'>
             <div className='max-w-52 sm:max-w-64 md:max-w-80 md:mr-10'>
-                <Image className='block w-full h-full' alt='vaka logo' width={200} height={200} src='https://vakacoffee.com/wp-content/uploads/2023/03/14011221001-e1678730159971-1024x368.png'></Image>
+                <Image className='block w-full h-full' alt='vaka logo' width={200} height={200} src='/banner-image.webp'></Image>
             </div>
         </section>
     )
