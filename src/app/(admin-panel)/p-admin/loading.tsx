39014884832ad08603272bec40feff8f86c58456
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-12-26 03:17:47 +0330

    add loading for admin panel pages

---
+++ src/app/(admin-panel)/p-admin/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function loading() {
+  return (
+    <div className="bg-secondary h-96 rounded-lg flex items-center justify-center animate-pulse">
+        در حال بارگذاری ...
+    </div>
+  )
+}
+
+export default loading;
