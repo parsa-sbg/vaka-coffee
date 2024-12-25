0dff3a34ea019defd1606c80d28758409e395dde
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-12-26 03:26:05 +0330

    add loading for dashboard pages

---
+++ src/app/(other)/dashboard/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function loading() {
+  return (
+    <div className="h-96 flex items-center justify-center bg-secondary animate-pulse rounded-lg">
+        در حال بارگذاری ...
+    </div>
+  )
+}
+
+export default loading;
